ff4cbfec6c86a3134b139558e1c249bf139c3ba7
Ben Fox <benjosephfox@gmail.com>
2022-06-01 05:36:23 +0100

    Initial Commit

---
+++ App Practice/App_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_PracticeApp.swift
+//  App Practice
+//
+//  Created by Ben Fox on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct App_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
